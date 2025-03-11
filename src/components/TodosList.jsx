37908e40c2cbce1d5a7a69314c6c4c00687b35d4
Gabriel Richter Reynoso <garey91@gmail.com>
2025-03-11 17:56:39 +0100

    styling

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -8,27 +8,31 @@ let TodosList = () => {
 
     let { todos, setTodos } = useContext(TodosContext)
     let [showCreate, setShowCreate] = useState(false);
-    let [show,setShow] = useState(true);
+    let [show, setShow] = useState(true);
 
 
     return (
-        <div className="todos-container">
+        <>
+                <h2 id="header">Todos</h2>
+                <button id="create-btn" onClick={() => { setShowCreate(true); setShow(false) }}>+</button>
+
+
+
             {
                 showCreate && <CreateTodo setShowCreate={setShowCreate} setShow={setShow} />
             }
-            <h2 id="header">Todos</h2>
+
             {
                 show &&
                 <>
-                <ul className="todo-list">
-                {
-                    todos.map((todo, i) => <Todo todo={todo} index={i} setShowCreate={setShowCreate} key={i} />)
-                }
-            </ul>
-            <button id="create-btn" onClick={() => {setShowCreate(true); setShow(false)}}>+</button>
+                    <ul className="todo-list">
+                        {
+                            todos.map((todo, i) => <Todo todo={todo} index={i} setShowCreate={setShowCreate} key={i} />)
+                        }
+                    </ul>
                 </>
             }
-        </div>
+        </>
     )
 }
 
